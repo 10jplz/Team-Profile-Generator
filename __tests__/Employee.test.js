f9a5bf399a988d780f2edac90862359acc6579e3
Jose Contreras <10jplz@Gmail.com>
2021-07-09 04:13:49 -0600

    UWorked on employee Tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,28 @@
+const { describe, expect, it } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
-describe("Employee", () => {
     describe("name", () => {
-        it("Should return an Employees Name",)
+        it("Should return new employee Name", () => {
+            const eName = "Jose";
+            const nE = new Employee("", eName);
+            expect(nE.name).toBe("Jose")
+            console.log(Employee);
+            console.log(eName);
+            console.log(nE);
+        })
     })
+
+describe("employee", () => {
+    describe("id", () => {
+        it("Should return new employee id", () => {
+            const eId = "101";
+            const nE = new Employee(eId);
+            expect(nE.id).toBe("101")
+            console.log(Employee);
+            console.log(eId);
+            console.log(nE);
+        })
+    })
+
+
 })
