158c308bd1da7a030c30b355a6a80c6da74307a0
Jose Contreras <10jplz@Gmail.com>
2021-07-10 04:01:37 -0600

    inquirer can create Manager, Engineer, Intern using their classes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // const generateMarkdown = require('./src/generateMarkdown');
-const Manager = require ('./lib/Manager');
-const Engineer = require ('./lib/Engineer');
-const Intern = require ('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 function addManager() {
-    inquirer.prompt ([
+    inquirer.prompt([
         {
-            type: 'input', 
+            type: 'input',
             name: 'id',
             message: "What is the Manager's Id?"
         },
@@ -29,9 +29,96 @@ function addManager() {
             message: "What is the Managers Office Number?"
         },
     ]).then((response) => {
-        let newManager = new Manager(response.id, response.name, response.email, response.officeNumber);
-        console.log(newManager);
+        let newEmployee = new Manager(response.id, response.name, response.email, response.officeNumber);
+
+        // const generateCard = generateHTML(newEmployee);
+        // fs.writeFileSync("./src/store.js", `\n${generateCard}`);
+        console.log(newEmployee)
+
+        addEmployee();
     })
 }
 
 addManager();
+
+function addEmployee(){
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employeeType',
+                message: 'Which type of employee would you like to add to the team?',
+                choices: ["Engineer", "Intern", "Finish"],
+            },
+        ]).then((response) => {
+            if (response.employeeType === "Engineer") {
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'id',
+                            message: "What is the Engineer's Id?"
+                        },
+                        {
+                            type: 'input',
+                            name: "name",
+                            message: "Please enter the Engineer's Name"
+                        },
+                        {
+                            type: 'input',
+                            name: 'email',
+                            message: "What is the Engineer's Email?"
+
+                        },
+                        {
+                            type: 'input',
+                            name: 'githubUser',
+                            message: "What is the Engineers github user name?"
+                        }
+                    ]).then((resonse) => {
+                        let newEmployee = new Engineer(resonse.id, response.name, response.email, resonse.githubUser)
+                        // const generateCard = generateHTML(newEmployee);
+                        // fs.writeFileSync("./src/store.js", `\n${generateCard}`);
+                        console.log(newEmployee)
+                        addEmployee();
+
+                    })
+            }
+            else if (response.employeeType === "Intern") {
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'id',
+                            message: "What is the Intern's Id?"
+                        },
+                        {
+                            type: 'input',
+                            name: "name",
+                            message: "Please enter the Intern's Name"
+                        },
+                        {
+                            type: 'input',
+                            name: 'email',
+                            message: "What is the Intern's Email?"
+
+                        },
+                        {
+                            type: 'input',
+                            name: 'githubUser',
+                            message: "What is the Intern's school?"
+                        }
+                    ]).then((response) => {
+                        let newEmployee = new Intern(response.id, response.name, response.email, response.school)
+                        // const generateCard = genarateHTML(newEmployee);
+                        // fs.writeFileSync("./src/store.js", `\n${generateCard}`);
+                        console.log(newEmployee)
+                        addEmployee();
+                    });
+
+            }
+            else {
+                console.log("Thank You! WE have created your team's profile")
+            }
+        })
+}
